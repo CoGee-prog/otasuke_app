c23e365acd63443d5d9aba661cac6ec2f630022e
CoGee-prog <bb.sca.ksk1@gmail.com>
2021-10-14 00:31:25 +0900

    EC2ユーザー変更設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,18 +9,14 @@ set :repo_url, "git@github.com:CoGee-prog/otasuke_app.git"
 append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
-set :ssh_options, {
-  auth_methods: ['publickey'], 
-  keys: ['~/.ssh/otasuke.pem'],
-	forward_agent: true
-}
+
 
 set :keep_releases, 3
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.7'
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/.unicorn.pid" }
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,7 @@
 server '13.114.166.139', user: 'kosuke', roles: %w{app db web}
+
+set :ssh_options, {
+  auth_methods: ['publickey'], 
+  keys: ['~/.ssh/otasuke_key_rsa'],
+	forward_agent: true
+}
