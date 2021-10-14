c7879995e653ad17b142f3c5a593666196b24754
CoGee-prog <bb.sca.ksk1@gmail.com>
2021-10-14 23:33:32 +0900

    CircleCIからCapistranoでデプロイ設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ server '13.114.166.139', user: 'kosuke', roles: %w{app db web}
 
 set :ssh_options, {
   auth_methods: ['publickey'], 
-  keys: ['~/.ssh/otasuke_key_rsa'],
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
 	forward_agent: true
 }
