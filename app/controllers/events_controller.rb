aa2826598d634d60102f4b8ce3e39a26a252e669
CoGee-prog <bb.sca.ksk1@gmail.com>
2021-12-22 14:58:24 +0000

    イベントコントローラーパフォーマンス改善途中

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
-    @events = @team.events
+    @events = @team.events.includes(event_entries: :event_option_entry)
   end
 
   def edit
